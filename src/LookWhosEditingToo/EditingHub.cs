ae4e21543d64b7f2226f8054e1af73c38c80b379
TimGeyssens <tg@umbraco.com>
2015-09-30 19:21:51 +0200

    Everything is working just need to cleanup the smelly code now :shit:

--- src/LookWhosEditingToo/EditingHub.cs
+++ src/LookWhosEditingToo/EditingHub.cs
@@ -7,14 +7,17 @@ namespace LookWhosEditingToo
     {
         public void Send(int nodeId, int userId)
         {
-            // Call the broadcasMessage method to update clients.
-            //var query = new Sql().Select("*").From("lookwhoseditingnow");
-            //var edits = ApplicationContext.Current.DatabaseContext.Database.Fetch<Edit>(query).Where(x => x.UserId != Umbraco.Web.UmbracoContext.Current.Security.CurrentUser.Id).ToList();
+            
             var user = ApplicationContext.Current.Services.UserService.GetUserById(userId);
             var userGravatar = Utility.HashEmailForGravatar(user.Email);
             var userName = user.Name;
 
             Clients.Others.broadcastEdit(nodeId, userId, userName, userGravatar);
+        }
+
+        public void Stop(int userId)
+        {
+            Clients.Others.broadcastStopEdit(userId);
         }
     }
 }
--- src/TestSitev7.2.8/App_plugins/LookWhosEditingToo/js/main.js
+++ src/TestSitev7.2.8/App_plugins/LookWhosEditingToo/js/main.js
@@ -1,7 +1,7 @@
 $(window).load(
     function () {
 
-        var allEdits;
+        var allEdits = [];
 
         // Declare a proxy to reference the hub. 
         var editing = $.connection.editingHub;
@@ -43,6 +43,32 @@ $(window).load(
             });
         }
 
+        function updateTreeAndPage() {
+
+            $("i[title*='content/content/edit']").closest("li").children("div").removeClass("look-whos-editing-too");
+
+            var currentNodeId = -1;
+            if (location.hash.indexOf('/content') == 1 && location.hash.indexOf('edit') != -1) {
+                var locArray = location.hash.split('/');
+                var currentNodeId = locArray[locArray.length - 1];
+            }
+
+            $("#look-whos-editing-too").empty();
+
+            for (var i = 0; i < allEdits.length; i++) {
+                var edit = allEdits[i];
+                $("i[title*='" + edit.nodeId + "']").closest("li").children("div").addClass("look-whos-editing-too");
+
+                if ($("#look-whos-editing-too").length == 0) {
+                    $("ng-form[name='contentNameForm']").parent().parent().children(".span5").prepend("<div id='look-whos-editing-too'></div>");
+                }
+
+                if ($("#look-whos-editing-too-" + edit.userId).length == 0 && currentNodeId == edit.nodeId)
+                    $("#look-whos-editing-too").append("<img id='look-whos-editing-too-" + edit.userId + "'src='//www.gravatar.com/avatar/" + edit.userGravatar + ".jpg?s=30&d=mm' alt='" + edit.userName + "' >");
+
+            }
+        }
+
         angular.element('#umbracoMainPageBody').scope().$on('$viewContentLoaded', function () {
 
             if (location.hash.indexOf('/content') == 1 && location.hash.indexOf('edit') != -1) {
@@ -70,32 +96,51 @@ $(window).load(
                 console.log($.cookie('lookWhosEditingTooUser') + ' not on content');
 
                 $.connection.hub.start().done(function () {
-                    editing.server.send(-1, $.cookie('lookWhosEditingNowUser'));
+                    console.log("send stop to server");
+                    var userId = parseInt($.cookie('lookWhosEditingTooUser').toString());
+                    console.log(userId);
+                    editing.server.stop(userId);
+                    editingResource.deleteByUserId(userId);
+                    console.log("done send stop to server");
                 });
 
-                editingResource.deleteByUserId($.cookie('lookWhosEditingNowUser'));
+                editingResource.deleteByUserId(0);
             }
         });
 
 
+        editing.client.broadcastStopEdit = function (userId) {
 
-        // Create a function that the hub can call to broadcast messages.
+            console.log("user " + userId + " is not on content anymore");
+
+            if (_.where(allEdits, { userId: userId }).length > 0) {
+
+                var currentUserEdit = _.where(allEdits, { userId: userId })[0];
+                allEdits.splice(_.indexOf(allEdits, currentUserEdit), 1);
+                console.log(_.indexOf(allEdits, currentUserEdit));
+                allEdits = _.reject(allEdits, function (el) { return el.userId === userId; });
+                console.log(allEdits);
+                updateTreeAndPage();
+            }
+
+        }
+
+
+
         editing.client.broadcastEdit = function (nodeId, userId, userName, userGravatar) {
 
 
             console.log("user " + userId + " is now on node " + nodeId);
 
-            $("i[title*='content/content/edit']").closest("li").children("div").removeClass("look-whos-editing-too");
+      
 
-            console.log("underscore testing");
-            if (_.where(allEdits, { userId: userId }).length > 0)
-            {
+        
+            if (_.where(allEdits, { userId: userId }).length > 0) {
                 //modify nodeId
                 var currentUserEdit = _.where(allEdits, { userId: userId })[0];
                 currentUserEdit.nodeId = nodeId;
             }
-            else
-            {
+            else {
                 //add new edit
                 var newUserEdit = {};
                 newUserEdit.nodeId = nodeId;
@@ -103,30 +148,10 @@ $(window).load(
                 newUserEdit.userName = userName;
                 newUserEdit.userGravatar = userGravatar;
 
-                allEdits.push(newUserEdit );
+                allEdits.push(newUserEdit);
             }
 
-            var currentNodeId = -1;
-            if (location.hash.indexOf('/content') == 1 && location.hash.indexOf('edit') != -1) {
-                var locArray = location.hash.split('/');
-                var currentNodeId = locArray[locArray.length - 1];
-            }
-
-            $("#look-whos-editing-too").empty();
-
-            for (var i = 0; i < allEdits.length; i++) {
-                var edit = allEdits[i];
-                $("i[title*='" + edit.nodeId + "']").closest("li").children("div").addClass("look-whos-editing-too");
-
-                if ($("#look-whos-editing-too").length == 0) {
-                    $("ng-form[name='contentNameForm']").parent().parent().children(".span5").prepend("<div id='look-whos-editing-too'></div>");
-                }
-
-                if ($("#look-whos-editing-too-" + edit.userId).length == 0 && currentNodeId == edit.nodeId)
-                    $("#look-whos-editing-too").append("<img id='look-whos-editing-too-" + edit.userId + "'src='//www.gravatar.com/avatar/" + edit.userGravatar + ".jpg?s=30&d=mm' alt='" + edit.userName + "' >");
-
-            }
-
+            updateTreeAndPage();
 
 
         };
