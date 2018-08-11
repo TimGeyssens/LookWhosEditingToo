c6d6dcc4d51c80ddbe1070c84ee19aa77df1b87f
Riadul Hoque <hoqueriadul@gmail.com>
2018-08-10 23:39:34 -0400

    Fixes for Umbraco v7.7
    Added $compile reference to the controller.js and changed the jQuery selector in main.js as .btn-group doesn't seem to exist in the new versions of Umbraco anymore

--- src/LookWhosEditingToo/App_Plugins/LookWhosEditingToo/js/main.js
+++ src/LookWhosEditingToo/App_Plugins/LookWhosEditingToo/js/main.js
@@ -4,10 +4,10 @@ $(window).load(
         var injector = angular.element('#umbracoMainPageBody').injector();
 
         function addWrapperAndLogic() {
-            if ($(".umb-panel-header").length) {
+            if ($(".umb-panel-header-content").length) {
                 if ($("#look-whos-editing-too-container").length == 0) {
                     //$(".umb-panel-header").find('.span5').append('<div id="look-whos-editing-too-container"></div>');
-                    $(".umb-panel-header").find('.btn-group.pull-right').prepend('<div id="look-whos-editing-too-container"></div>');
+                    $(".umb-panel-header-content").find('.umb-editor-header__actions-menu').prepend('<div id="look-whos-editing-too-container"></div>');
                     var html = $('<div id="look-whos-editing-too" ng-controller="LWET.ContentController"></div>');
                     injector.invoke(function ($compile) {
                         var obj = $('#look-whos-editing-too-container'); // get wrapper
